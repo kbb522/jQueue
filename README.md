0380cbf4218e1007b632ed7a24eac83e2b63d621
kbb522 <kbb@qq.com>
2017-07-24 10:20:57 +0800

    standard be added.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #jQuery
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
--- src/index.js
+++ src/index.js
@@ -5,57 +5,54 @@ JQueue队列，一款简易队列小应用
 时间：2017-7-21
 */
 class JQueue {
+  constructor (maxSize) {
+    this.queue = []
+    this.maxSize = maxSize || 5
+  }
 
-    constructor(maxSize) {
-        this.queue = []
-        this.maxSize = maxSize
-            ? maxSize
-            : 5
+  inqueue (obj) {
+    if (!obj) {
+      throw new Error('you must input a object for params. ps: no null, no undefined')
     }
-
-    inqueue(obj) {
-        if (!obj) {
-            throw new Error("you must input a object for params. ps: no null, no undefined")
-        }
-        if (this.queue.length >= this.maxSize) {
-            let t = this.queue.length - this.maxSize
-            t=t+1
+    if (this.queue.length >= this.maxSize) {
+      let t = this.queue.length - this.maxSize
+      t = t + 1
             // 裁剪数组，为加入的数据腾空间
-            this.queue.splice(0, t)
-        }
-        return this.queue.push(obj)
+      this.queue.splice(0, t)
     }
-    outqueue() {
-        return this.queue.length > 0
+    return this.queue.push(obj)
+  }
+  outqueue () {
+    return this.queue.length > 0
             ? this.queue.shift()
             : null
-    }
-    first() {
-        return this.queue.length > 0
+  }
+  first () {
+    return this.queue.length > 0
             ? this.queue[0]
             : null
-    }
-    last() {
-        return this.queue.length > 0
+  }
+  last () {
+    return this.queue.length > 0
             ? this.queue[this.queue.length - 1]
             : null
-    }
-    empty() {
-        return this.queue.length = 0
-    }
-    all() {
-        return this.queue
-    }
-    toString() {
-        let string = ""
-        this.queue.map(function(element, index, arr) {
-            string += element.toString()
-        })
-        return string
-    }
-    length() {
-        return this.queue.length
-    }
+  }
+  empty () {
+    return this.queue.length = 0
+  }
+  all () {
+    return this.queue
+  }
+  toString () {
+    let string = ''
+    this.queue.map(function (element, index, arr) {
+      string += element.toString()
+    })
+    return string
+  }
+  length () {
+    return this.queue.length
+  }
 }
 
 export default JQueue
